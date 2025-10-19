16bc52f7b292ac3fa12fbba5133af9f6f8c0c4c9
Sean Mafnas <seanvincent671@gmail.com>
2025-10-19 10:34:28 +1000

    Refactor fetch calls for improved readability and consistency

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
-    })
-    .catch(err => {
-        // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
-)`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
-    })
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    // Use a default background image/author
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
 
 fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-    .then(res => {
-        if (!res.ok) {
-            throw Error("Something went wrong")
-        }
-        return res.json()
-    })
-    .then(data => {
-        document.getElementById("crypto-top").innerHTML = `
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
             <img src=${data.image.small} />
             <span>${data.name}</span>
-        `
-        document.getElementById("crypto").innerHTML += `
+        `;
+    document.getElementById("crypto").innerHTML += `
             <p>🎯: $${data.market_data.current_price.usd}</p>
-            <p>👆: $${data.market_data.high_24h.usd}</p>
+              <p>👆: $${data.market_data.high_24h.usd}</p>
             <p>👇: $${data.market_data.low_24h.usd}</p>
-        `
-    })
-    .catch(err => console.error(err))
+        `;
+  })
+  .catch((err) => console.error(err));
 
-// function getCurrentTime() {
-//     const date = new Date()
-//     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
-// }
+function getCurrentTime() {
+  const date = new Date();
+  document.getElementById("time").textContent = date.toLocaleTimeString(
+    "en-us",
+    { timeStyle: "short" }
+  );
+}
 
-// setInterval(getCurrentTime, 1000)
+setInterval(getCurrentTime, 1000);
 
-
-/**
- * Challenge: Learn how to access the user's coordinates
- * by using the Geolocation Web API!
- * 
- * Log the user's position to the console.
- */
+navigator.geolocation.getCurrentPosition((pos) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&units=imperial&appid=e19d26fa0f9ab22b85ddf6d82192cf53`
+  )
+    .then((res) => res.json())
+    .then((position) => console.log(position));
+});
